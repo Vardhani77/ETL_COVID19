97b6332a0bb7caf773eed82022a32aa5dbffcc44
Sai Vardhani Ramisetti <sai@Sai-MacBook-Air.local>
2023-11-22 21:42:03 -0500

    changes visualize code

--- visualize.py
+++ visualize.py
@@ -1,99 +1,18 @@
-#!/usr/bin/env python
-# coding: utf-8
-
-# In[5]:
-
-
 import pandas as pd
-import seaborn as sns
-import matplotlib.pyplot as plt
+import plotly.express as px
 
-
-# In[ ]:
-
-
-
-
-
-# In[6]:
-
-
+# Load the data
 final_df = pd.read_csv('./transformed_data.csv')
-final_df.head(15)
 
-
-# In[ ]:
-
-
-
-
-
-# In[7]:
-
-
+# Visualization 1: Scatter plot for Cumulative Cases over Time
 values1 = ['India', 'United States of America', 'China']
-
 graph1_data = final_df[final_df['Country'].isin(values1)]
 
-
-# In[ ]:
-
-
-
-
-
-# In[8]:
-
-
-sns.set(style="whitegrid")  # Set the style (optional)
-g = sns.catplot(
-    x='Date_reported', y='Cumulative_cases', hue='Country',
-    data=graph1_data, kind='bar',
-    height=6, aspect=1.5  # Adjust the size and aspect ratio
+fig1 = px.scatter(
+    graph1_data, x='Date_reported', y='Cumulative_cases', color='Country',
+    labels={'Cumulative_cases': 'Cumulative Cases'},
+    title='Cumulative Cases Over Time'
 )
 
-
-# In[ ]:
-
-
-
-
-
-# In[10]:
-
-
-values2 = ['India', 'United States of America', 'China','Romania', 'South Africa', 'Australia']
-
-graph2_data = final_df[final_df['Country'].isin(values2)]
-
-
-# In[ ]:
-
-
-
-
-
-# In[11]:
-
-
-a = graph2_data['Country'].unique()
-b = graph2_data['TOTAL_VACCINATIONS'].unique()
-
-ax = sns.barplot(x=a, y=b)
-ax.set_xticklabels(ax.get_xticklabels(), rotation=40, ha="right")
-
-plt.xlabel('Countries')
-plt.ylabel('Total_Vaccinations')
-
-
-# In[ ]:
-
-
-
-
-
-# In[ ]:
-
-
-print("visualized sucessfully!!!")
-
+# Save the scatter plot for Cumulative Cases over Time as an image file
+fig1.write_image("scatter_plot_cumulative_cases.png")
